0cbae181bd47afe5741f9315b3f12a919dfd27b1
salbifaza <salbifaza13@gmail.com>
2021-03-08 15:49:17 +0700

    menu login

--- app/Views/login.php
+++ app/Views/login.php
@@ -18,38 +18,46 @@ $session = session();
 $errors = $session->getFlashdata('errors');
 
 ?>
-<main class="form-signin">
-    <form>
-        <img src="<?= base_url("img/logo_collect.png") ?>" alt="" class="mb-4" width="72" height="57">
-        <?php if ($errors != null) : ?>
-            <div class="alert alert-danger" role="alert">
-                <h4 class="alert-heading">Terjadi Kesalahan</h4>
-                <hr>
-                <p class="mb-0">
-                    <?php
-                    foreach ($errors as $err) {
-                        echo $err . '<br>';
-                    }
-                    ?>
-                </p>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-9 col-md7 col-lg-5 mx-auto">
+            <div class="card card-signin my-5 ">
+                <div class="text-center">
+                    <br>
+                    <img src="<?= base_url("img/logo_collect.png") ?>" alt="" class="img-fluid center" width="72" height="57">
+                </div>
+                <div class="card-body mx-4" style="max-width: 25rem;">
+                    <h3 class="card-title text-center">Login</h3>
+                    <?php if ($errors != null) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <h4 class="alert-heading">Terjadi Kesalahan</h4>
+                            <hr>
+                            <p class="mb-0">
+                                <?php
+                                foreach ($errors as $err) {
+                                    echo $err . '<br>';
+                                }
+                                ?>
+                            </p>
 
+                        </div>
+                    <?php endif ?>
+                    <?= form_open('Auth/login') ?>
+                    <div class="form-group">
+                        <?= form_label("Username", "username") ?>
+                        <?= form_input($username) ?>
+                    </div>
+                    <div class="form-group">
+                        <?= form_label("Password", "password") ?>
+                        <?= form_password($password) ?>
+                    </div>
+                    <div class="text-right">
+                        <?= form_submit('submit', 'Submit', ['class' => 'btn btn-primary']) ?>
+                    </div>
+                </div>
             </div>
-        <?php endif ?>
-        <?= form_open('Auth/login') ?>
-        <div class="form-group">
-            <?= form_label("Username", "username") ?>
-            <?= form_input($username) ?>
         </div>
-        <div class="form-group">
-            <?= form_label("Password", "password") ?>
-            <?= form_password($password) ?>
-        </div>
-        <div class="text-right">
-            <?= form_submit('submit', 'Submit', ['class' => 'btn btn-primary']) ?>
-        </div>
-    </form>
-
-</main>
-
+    </div>
+</div>
 <?= form_close() ?>
 <?= $this->endSection() ?>
